1c8e8a68732825ad2d071300ef4ddd42958064c1
miguel nuñez <miguelnuneze@gmail.com>
2023-07-07 20:18:09 -0500

    portfolio

---
+++ semana05/dia02-js/app.js
@@ -0,0 +1,62 @@
+console.log ('Hola Java Script')
+console.log ('Hola Chulls')
+console.log (2+2) 
+
+
+//tipos de datos en javascript
+//valores numericos
+console.log(100+4);
+console.log(100*4);
+console.log(100/4);
+//modulo==> residuo
+
+console.log(10%3);
+
+//string
+console.log ("Hola mi nombre es Miguel Nuñez");
+console.log ("18");
+
+//typeof()
+//boooleans==>operadores comparativos(>,<,=)
+//true(verdadero)
+//false(false)
+
+console.log(typeof(3<5));
+console.log(10<15);
+//conversion automatica==>coercion de tipo
+console.log("5"-1);
+console.log('5'+1);
+console.log('7'+0);
+//variables
+console.log('Mike');
+
+let age=17;
+
+let nombre
+nombre='Angel'
+console.log(nombre);
+
+nombre='cesar'
+console.log(nombre);
+
+nombre='Lucas'
+console.log(nombre);
+
+const pais='Peru'
+console.log(pais);
+
+
+
+
+let pais1='Argentina'
+pais1='Holanda'
+
+console.log(pais1)
+
+let pais2='Uruguay'
+pais2='suiza'
+
+console.log(pais2)
+
+
+
